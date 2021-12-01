4422b60b005f80dbb93962ca29238272a6cfc0ba
RaraImutBanget <95358465+RaraImutBanget@users.noreply.github.com>
2021-12-02 02:23:13 +0700

    Update allpayment.js

--- shop/allpayment.js
+++ shop/allpayment.js
@@ -1,23 +1,15 @@
 const allpayment = (prefix) => {
 return `PAYMENT BY ZEEONE OFC
-
-*さ BANK*
-1. BRI 
-	ㅁ 5074-01-003141-52-8
-		• R***A*DI
 		
 *さ E-MONEY*
-1. GOPAY
-	ㅁ 0887 4350 47326
-		• ZE**** **C
-2. OVO
-	ㅁ 0887 4350 47326
-		• EL*****TI
-3. DANA
-	ㅁ 0887 4350 47326
-		• ZE**** **C
+1. PULSA
+	ㅁ 0882 7204 5051
+		• Rara
 
-*さ PULSA*
+2. DANA
+	ㅁ 0882 7204 5051
+		• Rara
+
 
 Tidak tersedia
 	
